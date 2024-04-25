9518296f8806ca0f84f7236e532acf9f71beebe5
yun_kyoung_bae <daniel_1006@naver.com>
2024-04-25 17:21:28 +0900

    0425 (PM) 윤경배 - 1:1문의하기 리스트 , 문의하기 작성폼

--- src/main/java/com/beans/market/admin/controller/QuestionController.java
+++ src/main/java/com/beans/market/admin/controller/QuestionController.java
@@ -1,16 +1,28 @@
 package com.beans.market.admin.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.beans.market.admin.service.QuestionService;
 
 @Controller
 public class QuestionController {
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
 	
-	
+	@Autowired QuestionService questionService;
 	
 	//1:1 문의하기 페이지
 	@RequestMapping(value="/question")
@@ -19,18 +31,44 @@ public class QuestionController {
 		return "customerService/inquire";
 	}
 	
+	@RequestMapping(value="/question.ajax")
+	@ResponseBody
+	public Map<String, Object> questionList(String page) {
+		logger.info("questionList 요청");
+		logger.info("요청페이지 : " + page);
+		int currPage = Integer.parseInt(page);
+		int pagePerCnt = 10;
+		Map<String, Object> map = questionService.list(currPage, pagePerCnt);
+		return map;
+	}
+	
 	
 	//상세보기
 	@RequestMapping(value="/question.detail")
 	public String questionDetail() {
-		logger.info("questionDeatil 페이지 요청");
+		logger.info("questionDetail 페이지 요청");
 		return "customerService/inquireDetail";
 	}
 	
 	//문의 작성 폼
-	@RequestMapping(value="/question.do")
-	public String questionDo() {
-		logger.info("questionDo 페이지 요청");
-		return "customerService/inquireDetail";
+	@RequestMapping(value="/questionForm")
+	public String questionGo() {
+		logger.info("questionForm 페이지 요청");
+		return "customerService/inquireWrite";
 	}
+	
+	@RequestMapping(value="/question/write", method = RequestMethod.POST)
+    public String questionWrite (MultipartFile photos, HttpSession session, @RequestParam Map<String, String> param) {
+        logger.info("문의 글 작성");
+        logger.info("param : {} " , param);
+        String page = "redirect:/question";
+        
+        if(session.getAttribute("loginId")!=null) {
+        	int row = questionService.write(photos, param);
+        	if(row<1) {
+        		page= "questionForm";
+        	}
+        }
+        return page;
+}
 }
---
+++ src/main/java/com/beans/market/admin/dto/QuestionDTO.java
@@ -0,0 +1,59 @@
+package com.beans.market.admin.dto;
+
+import java.sql.Date;
+
+public class QuestionDTO {
+
+	private int inquiry_idx;
+	private String inquiry_pw;
+	private String inquiry_title;
+	private String inquiry_account;
+	private Date reg_date;
+	private String enquirer;
+	private String category_idx;
+	
+	public int getInquiry_idx() {
+		return inquiry_idx;
+	}
+	public void setInquiry_idx(int inquiry_idx) {
+		this.inquiry_idx = inquiry_idx;
+	}
+	public String getInquiry_pw() {
+		return inquiry_pw;
+	}
+	public void setInquiry_pw(String inquiry_pw) {
+		this.inquiry_pw = inquiry_pw;
+	}
+	public String getInquiry_title() {
+		return inquiry_title;
+	}
+	public void setInquiry_title(String inquiry_title) {
+		this.inquiry_title = inquiry_title;
+	}
+	public String getInquiry_account() {
+		return inquiry_account;
+	}
+	public void setInquiry_account(String inquiry_account) {
+		this.inquiry_account = inquiry_account;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getEnquirer() {
+		return enquirer;
+	}
+	public void setEnquirer(String enquirer) {
+		this.enquirer = enquirer;
+	}
+	public String getCategory_idx() {
+		return category_idx;
+	}
+	public void setCategory_idx(String category_idx) {
+		this.category_idx = category_idx;
+	}
+
+	
+}
